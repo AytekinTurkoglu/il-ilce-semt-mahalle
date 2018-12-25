96ad839bc51c14ff4def3f14c7dc0fb2faa9f36b
Semih Ferik <feriksemih@gmail.com>
2018-12-26 00:43:21 +0300

    :smiling_imp: :smiling_imp: :smiling_imp: 06.11.2018 tarihli aktarilmis sql dosyası eklendi.

--- src/ptt-pk/fetch.py
+++ src/ptt-pk/fetch.py
@@ -10,7 +10,7 @@ connection = pymysql.connect(host='localhost',
 cursor = connection.cursor()
 
 try:
-    wb = load_workbook('data/pk_list_31.08.2018.xlsx')
+    wb = load_workbook('data/pk_list_06.11.2018.xlsx')
     ws = wb.active
 	
     lower_map = { ord(u'I'): u'ı' }
