190e8eb4394aa57c53e648664653d6ce001bd5e7
Semih Ferik <feriksemih@gmail.com>
2019-12-04 21:14:32 +0300

    :snowflake: :snowflake: :snowflake: 02.12.2019 tarihli aktarilmis sql dosyası eklendi.

--- src/ptt-pk/fetch.py
+++ src/ptt-pk/fetch.py
@@ -10,7 +10,7 @@ connection = pymysql.connect(host='localhost',
 cursor = connection.cursor()
 
 try:
-    wb = load_workbook('data/pk_list_04.11.2019.xlsx')
+    wb = load_workbook('data/pk_list_02.12.2019.xlsx')
     ws = wb.active
 	
     lower_map = { ord(u'I'): u'ı' }
