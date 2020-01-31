d9be593062719601fc26a259881d255666fb67f5
Semih Ferik <feriksemih@gmail.com>
2020-01-31 21:26:13 +0300

    :syringe: :syringe: :syringe: 31.01.2020 tarihli aktarilmis sql dosyası eklendi.

--- src/ptt-pk/fetch.py
+++ src/ptt-pk/fetch.py
@@ -10,7 +10,7 @@ connection = pymysql.connect(host='localhost',
 cursor = connection.cursor()
 
 try:
-    wb = load_workbook('data/pk_list_03.01.2020.xlsx')
+    wb = load_workbook('data/pk_list_31.01.2020.xlsx')
     ws = wb.active
 	
     lower_map = { ord(u'I'): u'ı' }
